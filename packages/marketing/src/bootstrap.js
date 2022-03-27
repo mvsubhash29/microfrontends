1fc73fdd4041b79e5af335abf48dfb1043660a2e
M V Subhash <mvsubhash29@gmail.com>
2022-03-27 10:42:06 +0530

    Routing logic added for container and subapps

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDom.render(
-    <App />,
+    <App history={history} />,
     el
   );
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 };
 // Mount function to start up the app
 
@@ -15,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing_dev_root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 // If we are in development and in isolation,
